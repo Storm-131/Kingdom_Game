c4dd510ae91bde92fa4f694468c7bfe505681935
Tom M <49946728+Storm-131@users.noreply.github.com>
2023-12-06 17:01:47 +0100

    Refactor game logic and imports

---
+++ docs/ToDo.md
@@ -0,0 +1,37 @@
+# To Do ✍🏼
+
+---------------------------------------------
+
+### 🏗️ Under contructions
+- [ ] Refactoring der allgemeinen Funktionen in eigene Klassen
+  - [ ] Klasse für die einzelnen Modelle
+
+---
+### 🌵 List of To Do
+- [ ] Komplexität erhöhen
+  - [ ] Strategie, wenn Gegner mehr Soldaten hat und immer angreift
+  - [ ] Limitations for actions (e.g. soldiers become less effective if used for attacking multiple times in a row without rest or training)
+- [ ] Train with both players starting first (at random)
+- [ ] Eligibility Traces
+
+---
+### 🌵 Different Algorithms
+- [ ] SARSA
+- [ ] DQN + Experience Replay
+- [ ] PPO
+- [ ] MuZero
+
+---
+### Further ideas:
+- [ ] Add randomness to the game (e.g. random events)
+- [ ] Fine-Tune Hyperparameters & Discretization
+
+---------------------------------------------
+
+
+
+
+
+
+
+
--- main.py
+++ main.py
@@ -1,152 +1,15 @@
 # ---------------------------------------------------------*\
-# Title: Castle Game
+# Title: Kingdom Game
 # Author: TM
 # ---------------------------------------------------------*/
 #!/usr/bin/env python3
 
-from src.Kingdom import Kingdom
+import numpy as np
+from models.model import train_q_learning, evaluate_agent
 from src.simulate import simulated_game
 from utils.helpers import analyze_q_table
-from src.model import train_q_learning, discretize_state, get_state_index, choose_action, evaluate_agent
-import time
-import random
+from src.game import game
 
-import numpy as np
-
-#---------------------------------------------------------*/
-# Classical Game (Against other human)
-#---------------------------------------------------------*/
-def play_turn(player, opponent, choice):
-
-    if choice == 1:
-        gathered = player.gather_resources()
-        print(f"\n{player.name} gathered {gathered} resources.")
-        
-    elif choice == 2:
-        trained = player.train_soldiers()
-        if trained:
-            print(f"\n{player.name} trained {trained} soldiers.")
-        else:
-            print("\nNot enough resources to train soldiers.")
-            
-    elif choice == 3:
-        fortified = player.fortify_castle()
-        if fortified:
-            print(f"\n{player.name}'s castle has been fortified by {fortified}.")
-        else:
-            print("\nNot enough resources to fortify the castle.")
-
-    elif choice == 4:
-        success, damage = player.attack(opponent)
-        if success:
-            print(f"\n{player.name} successfully attacked! {opponent.name} " +
-                  f"lost {damage} castle strength and {damage} soldier(s).")
-        else:
-            print(f"\n{player.name}'s attack was repelled! {player.name} " +
-                f"lost {damage} soldier(s).")
-            
-def q_table_choice(kingdom1, kingdom2, q_table):
-    state1 = discretize_state(kingdom1)
-    state2 = discretize_state(kingdom2)
-    state_index = get_state_index(state1, state2)
-    return 1 + choose_action(q_table, state_index, epsilon=0)
-    
-def show_options_and_stats(player, opponent):
-    """
-    Display a friendly opening greeting with a cool ASCII header for the "Kingdom Game".
-    This function shows the player's stats and available actions.
-    """
-    
-    print("---------------------------------------------")
-    print(f"\n{player.name}'s Turn:")
-    print(f"Castle Strength: {player.castle_strength} (vs {opponent.castle_strength})")
-    print(f"Resources: {player.resources} (vs {opponent.resources})")
-    print(f"Soldiers: {player.soldiers} (vs {opponent.soldiers})")
-    print("\nActions:")
-    print("1. Gather Resources")
-    print("2. Train Soldiers")
-    print("3. Fortify Castle")
-    print("4. Attack")
-    
-
-def game(limit=50, opponent="human"):
-    
-    kingdom1 = Kingdom("Kingdom Atlantis")
-    kingdom2 = Kingdom("Kingdom Babylon", player=opponent)
-    Kingdoms = tuple(random.sample([kingdom1, kingdom2], 2))
-    
-    q_table = np.load("./out/q_table.npy")
-    turn = 1
-    print (ascii_header)
-    
-    print(f"\n{Kingdoms[0].name} ({Kingdoms[0].player}) vs {Kingdoms[1].name} ({Kingdoms[1].player})")
-    
-    while True:
-        print("\n---------------------------------------------")
-        print(f"Round {turn}")
-
-        # Player 1's turn
-        show_options_and_stats(Kingdoms[0], Kingdoms[1])
-        
-        if Kingdoms[0].player == "human":
-            choice = int(input("\nChoose your action: "))
-            play_turn(Kingdoms[0], Kingdoms[1], choice)
-            time.sleep(1.5)
-        else:
-            choice = q_table_choice(Kingdoms[0], Kingdoms[1], q_table)
-            time.sleep(2)
-            play_turn(Kingdoms[0], Kingdoms[1], choice)
-            time.sleep(2)
-            
-        if Kingdoms[1].castle_strength <= 0:
-            print(f"\n{Kingdoms[0].name} wins! 👑\n")
-            break
-        
-        # Player 2's turn
-        show_options_and_stats(Kingdoms[1], Kingdoms[0])
-        
-        if Kingdoms[1].player == "human":
-            choice = int(input("\nChoose your action: "))
-            play_turn(Kingdoms[1], Kingdoms[0], choice)
-            time.sleep(1.5)
-        else:
-            choice = q_table_choice(Kingdoms[1], Kingdoms[0], q_table)
-            time.sleep(2)
-            play_turn(Kingdoms[1], Kingdoms[0], choice)
-            time.sleep(2)
-            
-        if Kingdoms[0].castle_strength <= 0:
-            print(f"\n{Kingdoms[0].name} wins! 👑\n")
-            break
-        
-        # Check Turn Limit
-        turn += 1
-                
-        if turn >= limit:
-            print("\nGame ended in a draw!")
-            break
-
-# ASCII Art for "Kingdom Game"
-ascii_header = """
-   /\                                                        /\\
-  |  |                                                      |  |
- /----\              Welcome to the Kingdom-Game           /----\\
-[______]           ~ Where Brave Knights Tremble ~        [______]
- |    |         _____                        _____         |    |
- |[]  |        [     ]                      [     ]        |  []|
- |    |       [_______][ ][ ][ ][][ ][ ][ ][_______]       |    |
- |    [ ][ ][ ]|     |  ,----------------,  |     |[ ][ ][ ]    |
- |             |     |/'    ____..____    '\|     |             |
-  \  []        |     |    /'    ||    '\    |     |        []  /
-   |      []   |     |   |o     ||     o|   |     |  []       |
-   |           |  _  |   |     _||_     |   |  _  |           |
-   |   []      | (_) |   |    (_||_)    |   | (_) |       []  |
-   |           |     |   |     (||)     |   |     |           |
-   |           |     |   |      ||      |   |     |           |
- /''           |     |   |o     ||     o|   |     |           '\\
-[_____________[_______]--'------''------'--[_______]_____________]                                 
-    """
-    
 #---------------------------------------------------------*/
 # Main-Loop
 #---------------------------------------------------------*/
@@ -157,7 +20,7 @@ if __name__ == "__main__":
     
     # 1) Nornal game (against human or machine)
     if choice == 1:  
-        game(opponent="machine")  # human / machine
+        game(opponent="machine")  # human / machine1
     
     # 2) Simulated game
     elif choice == 2: 
